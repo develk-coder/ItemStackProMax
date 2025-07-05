3c94baf01979000bf67f56b86646b9129a9c40e3
develk <develk@163.com>
2025-07-05 19:43:04 +0800

    init

---
+++ src/main/java/develk/itemstackpromax/ItemStackProMax.java
@@ -0,0 +1,10 @@
+package develk.itemstackpromax;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ItemStackProMax implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/develk/itemstackpromax/client/ItemStackProMaxClient.java
@@ -0,0 +1,10 @@
+package develk.itemstackpromax.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemStackProMaxClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}

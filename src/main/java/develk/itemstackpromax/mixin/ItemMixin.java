4a19221acfeda0fba7313859ff904691e9241657
develk <develk@163.com>
2025-07-08 01:27:40 +0800

    fix 1.21.5

--- src/main/java/develk/itemstackpromax/mixin/ItemMixin.java
+++ src/main/java/develk/itemstackpromax/mixin/ItemMixin.java
@@ -1,5 +1,6 @@
 package develk.itemstackpromax.mixin;
 
+import net.minecraft.component.type.TooltipDisplayComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.tooltip.TooltipType;
@@ -9,14 +10,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
+import java.util.function.Consumer;
 
 @Mixin(Item.class)
 public abstract class ItemMixin {
     @Inject(method = "appendTooltip", at = @At("HEAD"))
-    private void StackCountTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type, CallbackInfo ci) {
+    private void StackCountTooltip(ItemStack stack, Item.TooltipContext context, TooltipDisplayComponent displayComponent, Consumer<Text> textConsumer, TooltipType type, CallbackInfo ci) {
         if (stack.getCount() >= 1000) {
-            tooltip.add(Text.translatable("tooltip.itemstackpromax.stackcount", stack.getCount()));
+            textConsumer.accept(Text.translatable("tooltip.itemstackpromax.stackcount", stack.getCount()));
         }
     }
 }
